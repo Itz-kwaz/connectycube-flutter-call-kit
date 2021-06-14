d46e625fa64cb122916c13a3817060f98339345b
Chidubem Ovbiebo <50139585+ovbiebo@users.noreply.github.com>
2021-06-14 12:19:48 +0100

    testing logging

--- android/src/main/kotlin/com/connectycube/flutter/connectycube_flutter_call_kit/CallForegroundService.kt
+++ android/src/main/kotlin/com/connectycube/flutter/connectycube_flutter_call_kit/CallForegroundService.kt
@@ -3,6 +3,7 @@ package com.connectycube.flutter.connectycube_flutter_call_kit
 import android.app.Service
 import android.content.Intent
 import android.os.IBinder
+import android.util.Log
 import com.connectycube.flutter.connectycube_flutter_call_kit.*
 
 
@@ -12,8 +13,17 @@ class CallForegroundService : Service(){
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-//        return super.onStartCommand(intent, flags, startId)
+        super.onStartCommand(intent, flags, startId)
         val extras = intent?.extras
+
+        if(intent != null){
+            Log.d("CallForeground", "intentttttttttttttttttttttttttttttttttttt")
+        }
+
+        if(extras != null){
+            Log.d("CallForeground", "extrassssssssssssssssssssssssssssssssssss")
+            Log.d("CallForeground", extras?.getString("call_id") as String)
+        }
 
         val callId = extras?.getString("call_id") as String
         val callType = extras.getInt("call_type") as Int
